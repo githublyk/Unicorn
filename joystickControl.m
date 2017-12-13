e30eb024d532690d7d6bb1a852371fe3792a5057
Saumya <saxena.saumya92@gmail.com>
2017-12-12 22:38:29 -0500

    Saumya has finally done some work

--- joystickControl.m
+++ joystickControl.m
@@ -37,13 +37,14 @@ startup;
 end
 
 function joyCmd = mapJoystick(axes, buttons)
-    axes = axes .* (abs(axes)>.2)
+    axes = axes .* (abs(axes)>.2);
     joyCmd.x = -axes(1);
     joyCmd.y = axes(2);
     joyCmd.z = buttons(6) - buttons(5);
     joyCmd.roll = buttons(8) - buttons(7);
     joyCmd.pitch = -axes(4);
     joyCmd.yaw = axes(3);
+    joyCmd.mode = buttons(7);
 end
 
 function cartMotion = calculateMotion(joyCmd, cartMotion, dt)
@@ -59,20 +60,25 @@ function cartMotion = calculateMotion(joyCmd, cartMotion, dt)
     roll = 0;
     pitch = joyCmd.pitch*dt*2;
     yaw = joyCmd.yaw*dt*2;
-    cartMotion = [xyz, roll, pitch, yaw];
+    mode = joyCmd.mode;
+    cartMotion = [xyz, roll, pitch, yaw, mode];
 end
 
 
 function updateFodbot(fodbot, dP)
     dP(5) = .5*dP(5);
     dP(6) = -.5*dP(6);
-    PITCH = [cos(dP(5)) -sin(dP(5)) 0;
-                 sin(dP(5)) cos(dP(5)) 0;
+  
+    thetap = dP(6)*(1-dP(7));
+    thetay = dP(5)*(1-dP(7));
+    PITCH = [cos(thetap) -sin(thetap) 0;
+                 sin(thetap) cos(thetap) 0;
                  0 0 1];
-    YAW = [cos(dP(6)) 0 sin(dP(6));
+    YAW = [cos(thetay) 0 sin(thetay);
                  0 1 0;
-                -sin(dP(6)) 0 cos(dP(6))];
+                -sin(thetay) 0 cos(thetay)];
     ROT = YAW*PITCH*eye(3);
+%    dT = [ROT, [0; 0; 0]; [0,0,0,1]];
     dT = [ROT, [dP(1); dP(2); dP(3)]; [0,0,0,1]];
 %    dT = [eye(3), [dP(1); dP(2); dP(3)]; [0,0,0,1]];
     
@@ -80,20 +86,47 @@ function updateFodbot(fodbot, dP)
     fbk = fodbot.fullGroup.getNextFeedback();
     fk = fodbot.arm.getNominalFK();
     newFK = dT *fk;
-    
-    
+
     % angles = kin.getIK(...
     %     'xyz', newFK(1:3,4), ...
     %     'InitialPositions', fbk.positionCmd);
 
-    angles = kin.getIK(...
-        'xyz', newFK(1:3,4), ...
-        'InitialPositions', fbk.positionCmd, ...
-        'so3', newFK(1:3,1:3));
+        angles = kin.getIK(...
+            'xyz', newFK(1:3,4), ...
+            'InitialPositions', fbk.positionCmd, ...
+            'so3', newFK(1:3,1:3));
+      frames = kin.getForwardKinematics('output', fbk.position);
+      f = frames(1:3,1:3,end-1);
+%       for i = 1:size(angles)-1
+%           f = frames(1:3,1:3,i)*f;
+%       end
+%     angles(end-1) = angles(end-1) + (1-dP(7))*dP(6);
+%     angles(end) = angles(end) + (1-dP(7))*dP(5);
+    
+    alpha = atan(f(1,1)/f(2,1))*180/pi;
+    beta = atan(sqrt(f(3,2)^2 + f(3,3)^2)/(-f(3,1)))*180/pi;
+    gamma = atan(f(3,2)/f(3,3))*180/pi;
+    R =[alpha beta gamma]
+    alpha = alpha*pi/180;
+    beta = beta*pi/180;
+    gamma = gamma*pi/180;
+    
+     if abs(beta)>0 && abs(beta) <=pi/4
+        angles(end-1) = angles(end-1) - dP(7)*dP(6);
+        angles(end-3) = angles(end-3) + dP(7)*dP(6);
+        angles(end) = angles(end) + dP(7)*dP(5);
+        angles(end-2) = angles(end-2) - dP(7)*dP(5);
 
-%     angles(end-1) = angles(end-1) + dP(6);
-%     angles(end) = angles(end) + dP(5);
+      end
+    if abs(beta)>pi/4 && abs(beta) <=pi/2
+        angles(end-1) = angles(end-1) - dP(7)*dP(5);
+        angles(end-3) = angles(end-3) + dP(7)*dP(5);
+        angles(end) = angles(end) + dP(7)*dP(6);
+        angles(end-2) = angles(end-2) - dP(7)*dP(6);
+    end
+    
     fodbot.arm.setAngles(angles);
+
 end
 
 function runSpecialCommands(fodbot, buttons, joy)
